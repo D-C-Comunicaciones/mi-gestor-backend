846b9b9d23e8370ab04618a2857ca17b4c2a568b
afleones <afleones@icloud.com>
2025-10-09 14:42:55 -0500

    CHanges: Ajustes en interceptor de métricas. 2025-10-09 13:57

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AppService } from './app.service';
 
@@ -10,5 +10,10 @@ export class AppController {
   getHello(@Res() res: Response) {
     const html = this.appService.getHelloHtml(); // el service devuelve el HTML
     res.type('html').send(html); // el controller solo lo envía al cliente
+  }
+
+  @Get('simulate-error')
+  simulateError() {
+    throw new HttpException('Error simulado para testing', HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
