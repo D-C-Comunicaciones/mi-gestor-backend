0934254a150fa88d6de5e18b1887966c4d85d617
afleones <afleones@icloud.com>
2025-10-22 18:24:05 -0500

    Release: reporte de intereses moratorios listo. 2025-10-22 - 18:13.

--- prisma/migrations/20251022195142_add_timestamp_fields_to_moratory_interest_model/migration.sql
+++ prisma/migrations/20251022195142_add_timestamp_fields_to_moratory_interest_model/migration.sql
@@ -8,8 +8,8 @@
 DROP INDEX "public"."moratory_interests_installmentId_moratoryInterestStatusId_d_idx";
 
 -- AlterTable
-ALTER TABLE "public"."moratory_interests" ADD COLUMN     "createdAt" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "updatedAt" DATE NOT NULL;
+ALTER TABLE "public"."moratory_interests" ADD COLUMN "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN "updatedAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 -- CreateIndex
 CREATE INDEX "moratory_interests_installmentId_moratoryInterestStatusId_d_idx" ON "public"."moratory_interests"("installmentId", "moratoryInterestStatusId", "daysLate", "amount", "paidAt", "paidAmount", "isPaid", "createdAt", "updatedAt");
